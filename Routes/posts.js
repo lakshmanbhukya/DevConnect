175e164d90210519707fbcd735fb831fafa5c931
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-06-29 11:50:01 +0530

    feat(posts): implement delete post by ID and secure addPost route

--- Routes/posts.js
+++ Routes/posts.js
@@ -11,8 +11,9 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Error fetching posts" });
   }
 });
-router.post("/addPost", async (req, res) => {
-  const { user, text, name, avatar } = req.body;
+router.post("/addPost",protected, async (req, res) => {
+  const user = req.user.id;
+  const { text, name, avatar } = req.body;
 
   if (!user || !text) {
     return res.status(400).json({ error: "User and text are required" });
@@ -60,6 +61,22 @@ router.put("/unlike/:id", protected, async (req, res) => {
     );
     await post.save();
     res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+router.delete("/delete/:id", protected, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    await post.deleteOne();
+    res.json({ msg: "Post removed" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
