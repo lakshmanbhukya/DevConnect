3d032ab64521a0151b097a799d6e33a0b03262e6
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-06-29 11:44:30 +0530

    refactor: remove non-functional code and test functions

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -9,19 +9,10 @@ function protected(req, res, next) {
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified.user;
-    // console.log(verified);
     next();
   } catch (err) {
     res.status(400).send("Invalid Token");
   }
 }
 module.exports = protected;
-// const token = "paste_jwt_here_and_chech_it";
-// const secret = "secret"; // exactly what's in your .env file
 
-// try {
-//   const decoded = jwt.verify(token, secret);
-//   console.log("✅ Token is valid:", decoded);
-// } catch (e) {
-//   console.error("❌ Invalid Token:", e.message);
-// }
